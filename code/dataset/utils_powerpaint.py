c6b65a9d7435e849c2a6de30c80269cca7fc86aa
Filippo <filippo99merlo@gmail.com>
2024-08-05 20:07:51 +0200

    Refactor generate_new_images function to include LLAVA object detection prompt and scale in generated image filenames

--- code/dataset/utils_powerpaint.py
+++ code/dataset/utils_powerpaint.py
@@ -641,9 +641,8 @@ def generate_new_images(data, image_names):
                 inputs_llava_1 = llava_processor(prompt_llava_1, return_tensors="pt").to(LLAVA_DEVICE)
                 output_llava_1 = llava_model.generate(**inputs_llava_1, max_new_tokens=70)
                 full_output_llava_1 = llava_processor.decode(output_llava_1[0], skip_special_tokens=True)
-                print(full_output_llava_1)
-
                 prompt = f"{art} {object_for_replacement.replace('/',' ').replace('_',' ')}. " + full_output_llava_1.replace(prompt_llava_1, "")
+                print(prompt)
                 shape_guided_prompt = prompt
                 shape_guided_negative_prompt = 'humans, people, person, body, face, head, hands, legs, arms, torso, skin, eyes, mouth, fingers, feet, hair, human-like figures, silhouettes, limbs, human anatomy, human features, mannequins, dolls, humanoid shapes'
                 fitting_degree = 0.6 # 0-1
