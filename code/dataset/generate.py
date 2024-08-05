0b3ba29f1a4443ea0041a68ef31281ba126d6c97
Filippo <filippo99merlo@gmail.com>
2024-08-05 15:52:41 +0200

    sss

--- code/dataset/generate.py
+++ code/dataset/generate.py
@@ -5,5 +5,5 @@ from dataset import *
 if __name__ == '__main__':
     dataset = Dataset(dataset_path = dataset_path)
     data = dataset.data
-    image_names = dataset.img_names[:10]
+    image_names = dataset.img_names[:100]
     generate_new_images(data, image_names)
